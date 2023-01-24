4c9d567155ca472896b9cf34d128e92151a05b38
TDiaz83 <tomas.enrique.diaz@gmail.com>
2023-01-24 14:31:53 -0600

    Updated BrowserRouter bug

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Header from '../src/components/Header'
 import Footer from './components/Footer';
 
